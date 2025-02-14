e9cd482de9dfbbebdf32a6ba617592a20736d4f0
Mark Krasner <mzkrasner@gmail.com>
2025-02-14 13:37:34 -0500

    Adding in price getter action for sim

--- src/plugin-simulator-solana/index.ts
+++ src/plugin-simulator-solana/index.ts
@@ -2,11 +2,12 @@ import { Plugin } from '@elizaos/core';
 import { TradingService } from './services/trading.service.ts';
 import { executeTradeAction } from './actions/place-trade.ts';
 import { getBalancesAction } from './actions/get-balances.ts';
+import { getTokenPriceAction } from './actions/get-price.ts';
 
 export const tradingSimulatorPlugin: Plugin = {
   name: 'Trading Simulator Plugin',
   description: 'Simulates trading on Solana',
-  actions: [executeTradeAction, getBalancesAction],
+  actions: [executeTradeAction, getBalancesAction, getTokenPriceAction],
   //   evaluators: [knowledgeEvaluator],
   providers: [],
   services: [TradingService.getInstance()],
